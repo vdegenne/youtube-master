4748b2ec21fcd90a5d723eaa93b1a9a08f79f07f
vdegenne <vdegenne@gmail.com>
2022-03-28 21:58:53 +0200

    minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import tsc from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
+import {terser} from 'rollup-plugin-terser'
 
 export default {
   input: 'src/main.ts',
   output: { file: 'docs/app.js', format: 'esm' },
-  plugins: [tsc(), resolve()]
+  plugins: [
+    tsc(), resolve(),
+    process.env.minify ? terser() : {}
+  ]
 }
